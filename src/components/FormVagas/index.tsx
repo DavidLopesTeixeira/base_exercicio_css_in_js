04f8b91c08d765e107b55b7ddf2658c51f6bb574
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-05-18 12:52:28 -0300

    conclusão do exercício , aplicação do styledComponents e remoção dos modules.css, global.css também foram removidas todas as importações dos respectivos módulos.

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,5 @@
 import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
+import { FormularioPesquisa, InputPesquisa, BotaoPesquisa } from './syles'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +14,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <FormularioPesquisa onSubmit={aoEnviarForm}>
+      <InputPesquisa
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <BotaoPesquisa type="submit">Pesquisar</BotaoPesquisa>
+    </FormularioPesquisa>
   )
 }
 export default FormVagas
